f577f7361a920a6072283c8397764f94e169492f
Moon kyuseok <msk7676@naver.com>
2026-01-28 14:36:21 +0900

    2-3

---
+++ 2-3.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int func(int *mod ,int *rem);
+
+int main(void) {
+
+	int mod, rem;
+	func(&mod, &rem);
+	printf("몫: %d, 나머지: %d\n", mod, rem);
+
+	return 0;
+
+}
+
+int func(int* mod, int* rem) {
+	int b=0;
+	printf("양수 입력: ");
+	scanf("%d",&b);
+	*mod = b / 4;
+	*rem = b % 4;
+	return 0;
+
+}
+
+
+
