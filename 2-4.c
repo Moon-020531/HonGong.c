64ce75a8b222257e1fc8c23f6723acba4fb6354c
Moon kyuseok <msk7676@naver.com>
2026-01-28 15:30:52 +0900

    2-5

---
+++ 2-4.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void rotate(int* pa, int* pb, int* pc);
+
+int main(void){
+	int a, b, c;
+	char d;
+	int result;
+	printf("세 정수 입력: ");
+	scanf("%d %d %d", &a, &b, &c);
+
+	for (int i = 0; i < 200; i++) {
+		scanf("%c", &d);
+		if (d == '\n') {
+			rotate(&a, &b, &c);
+			printf("%d %d %d", a, b, c);
+			continue;
+		}
+		else
+			break;
+		
+	}
+
+}
+
+void rotate(int* pa, int* pb, int* pc) {
+	int temp;
+	temp = *pc;
+	*pc = *pb;
+	*pb = *pa;
+	*pa = temp;
+
+}
