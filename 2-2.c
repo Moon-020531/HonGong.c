799d76367d9b0cc86ff115286f6a0a7a3251e5cd
Moon kyuseok <msk7676@naver.com>
2026-01-28 13:03:55 +0900

    반복문으로 수정

--- 2-2.c
+++ 2-2.c
@@ -5,59 +5,23 @@ int main(void) {
 	int i;
 	int a[6] = { 0 };
 	int result;
+	int z;
+	
 
 	for (i = 0; i < 6; i++)
 	{
 		printf("로또 번호 입력: ");
 		result = scanf("%d", &a[i]);
-
-	
-			if (a[i] == a[i - 1]) {
-
-				printf("중복된 숫자입니다");
-				printf("\n");
-				i--;
-				continue;
-
-
-			}
-			else if (a[i] == a[i - 2]) {
-
-				printf("중복된 숫자입니다");
-				printf("\n");
-				i--;
-				continue;
-
-
-			}
-			else if (a[i] == a[i - 3]) {
-
-				printf("중복된 숫자입니다");
-				printf("\n");
-				i--;
-				continue;
-
-
-			}
-			else if (a[i] == a[i - 4]) {
-
-				printf("중복된 숫자입니다");
-				printf("\n");
-				i--;
-				continue;
-
-			}
-			else if (a[i] == a[i - 5]) {
-
-				printf("중복된 숫자입니다");
-				printf("\n");
-				i--;
-				continue;
-
-
+			for (z = 0; z < i; z++) {
+				if (a[i] == a[z]) {
+					printf("중복된 숫자입니다");
+					printf("\n");
+					i--;
+					break;
+				}
 			}
 
-		}
+	}
 	
 	printf("입력된 번호:");
 	for (i = 0; i < 6; i++)
