f5856a897e21f820b3b7844738555bdf31431182
Moon kyuseok <msk7676@naver.com>
2026-01-30 10:36:01 +0900

    메뉴선택 기능

---
+++ vending machine.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+int main(void) {
+
+	int menu;
+	printf("=====자판기======\n");
+
+	printf("메뉴를 선택해주세요:\n");
+
+	printf("1.콜라(500원) 2.사이다(600원) 3.환타(700원)\n");
+	scanf("%d", &menu);;
+	printf("선택한 메뉴는 %d번입니다.\n", menu);
+
+
+
+
+
+
+}
